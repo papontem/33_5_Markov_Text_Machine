731b2c89af9e7a15ee3055a11b407e34007c85c3
papontemjetix <papontem@gmail.com>
2023-08-17 12:36:22 -0400

    completed unit tests for markov.js thus far. moving to step 2.

--- markov.test.js
+++ markov.test.js
@@ -4,7 +4,7 @@ describe("MakrovMachine Class Tests", function () {
     const testString = "i am a test string i"
 
     beforeEach(function () {
-        console.log("BEFORE EACH TOP TEST!");
+        console.log("BEFORE EACH CLASS TEST!");
     });
     
     // afterEach(function () {
@@ -21,14 +21,17 @@ describe("MakrovMachine Class Tests", function () {
 
     describe("Test Suite for markov machine's intitializing this.words' split", function () {
         
-        beforeEach(function () {
-            console.log("BEFORE EACH this.words BOTTOM TEST!");
+        beforeAll(function () {
+            console.log("BEFORE ALL this.words BOTTOM TEST!");
         });
+        // beforeEach(function () {
+        //     console.log("BEFORE EACH this.words BOTTOM TEST!");
+        // });
 
         test("Test for empty input string", function () {
             // Test Logic
             let input = testString.slice(0,0)
-            console.log("Test String:",input);
+            // console.log("Test String:",input);
             let mm = new MarkovMachine(input);
             // Expectations
             expect(mm.words).toEqual([]);
@@ -36,7 +39,7 @@ describe("MakrovMachine Class Tests", function () {
         test("Test for input string with one word", function () {
             // Test Logic
             let input = testString.slice(0,1)
-            console.log("Test String:",input);
+            // console.log("Test String:",input);
             let mm1 = new MarkovMachine(input);
             // Expectations
             expect(mm1.words).toEqual(["i"]);
@@ -47,7 +50,7 @@ describe("MakrovMachine Class Tests", function () {
             //  0123456789
             // "i am a test string i"
             let input = testString.slice(0,4)
-            console.log("Test String:",input);
+            // console.log("Test String:",input);
             let mm2 = new MarkovMachine(input);
             // console.log("words:", mm2.words);
             // Expectations
@@ -57,23 +60,26 @@ describe("MakrovMachine Class Tests", function () {
         test("Test for input string with multiple words", function () {
             // Test Logic
             let input = testString.slice(0,testString.length)
-            console.log("Test String:",input);
+            // console.log("Test String:",input);
             let mm3 = new MarkovMachine(input);
             // Expectations
             expect(mm3.words).toEqual(["i", "am", "a", "test", "string", "i"]);
         });
     });
 
-    describe("Test Suite for markov machine's this.makeChains succesfully build this.wordMapChains", function () {
+    describe("Test Suite for markov machine's this.makeChains succesfully builds this.wordMapChains", function () {
         
-        beforeEach(function () {
-            console.log("BEFORE EACH this.makeChains BOTTOM TEST!");
+        beforeAll(function () {
+            console.log("BEFORE ALL this.makeChains BOTTOM TEST!");
         });
+        // beforeEach(function () {
+        //     console.log("BEFORE EACH this.makeChains BOTTOM TEST!");
+        // });
 
         test("Test for expected map object with empty input string", function () {
             // Test Logic
             let mm = new MarkovMachine("");
-            console.log("mm's word chain map:",mm.wordMapChains);
+            // console.log("mm's word chain map:",mm.wordMapChains);
             // Expectations
             expect(mm.wordMapChains).toEqual(expect.any(Object));
             expect(mm.wordMapChains).toEqual({});
@@ -81,7 +87,7 @@ describe("MakrovMachine Class Tests", function () {
         test("Test for expected map object with string with one word", function () {
             // Test Logic
             let mm1 = new MarkovMachine("i");
-            console.log("mm1's word chain map:",mm1.wordMapChains);
+            // console.log("mm1's word chain map:",mm1.wordMapChains);
             // Expectations
             expect(mm1.wordMapChains).toEqual(expect.any(Object));
             expect(mm1.wordMapChains).toEqual({
@@ -92,7 +98,7 @@ describe("MakrovMachine Class Tests", function () {
         test("Test for expected map object with string with two words", function () {
             // Test Logic
             let mm2 = new MarkovMachine("i am");
-            console.log("mm2's word chain map:",mm2.wordMapChains);
+            // console.log("mm2's word chain map:",mm2.wordMapChains);
             // Expectations
             expect(mm2.wordMapChains).toEqual(expect.any(Object));
             expect(mm2.wordMapChains).toEqual({
@@ -104,9 +110,8 @@ describe("MakrovMachine Class Tests", function () {
         test("Test for expected map object with string with multiple words", function () {
             // Test Logic
             let mm3 = new MarkovMachine("i am a test string i");
-            console.log("mm3's word chain map:",mm3.wordMapChains);
+            // console.log("mm3's word chain map:",mm3.wordMapChains);
             // Expectations
-            // PAM BUG: FOR SOME REASON THE WORD AM IS NO TBEING MAPPED TO KEY WORD "i"
             expect(mm3.wordMapChains).toEqual(expect.any(Object));
             expect(mm3.wordMapChains).toEqual({
                      "i":["am" , null],
@@ -118,5 +123,55 @@ describe("MakrovMachine Class Tests", function () {
         });
     });
 
-    
+    describe("Test Suite for markov machine's makeText() function sucessfully creates text as expected",function(){
+
+        test("Test for makeText() to return expected string with 0 words",function(){
+            // Test Logic
+            let input = testString.slice(0,testString.length)
+            // console.log("Test String:",input);
+            let mm = new MarkovMachine(input);
+            let createdText = mm.makeText(0)
+            // console.log("createdText:",createdText);
+            // Expectations
+            expect(createdText).toEqual(expect.any(String))
+            expect(createdText).toEqual("")
+        });
+        test("Test for makeText() to return expected string with 1 word",function(){
+            // Test Logic
+            let input = testString.slice(0,testString.length)
+            // console.log("Test String:",input);
+            let mm = new MarkovMachine(input);
+            let createdText = mm.makeText(1)
+            // console.log("createdText:",createdText);
+            // Expectations
+            expect(createdText).toEqual(expect.any(String))
+            expect(createdText.split(/[ \r\n]+/).length).toEqual(1)
+        });
+        test("Test for makeText() to return expected string with 2 words",function(){
+            // Test Logic
+            let input = testString.slice(0,testString.length)
+            // console.log("Test String:",input);
+            let mm = new MarkovMachine(input);
+            let createdText = mm.makeText(2)
+            // console.log("createdText:",createdText);
+            // Expectations
+            expect(createdText).toEqual(expect.any(String))
+            expect(createdText.split(/[ \r\n]+/).length).toEqual(2)
+        });
+        test("Test for makeText() to return expected string with 100 words by default",function(){
+            // Test Logic
+            let input = testString.slice(0,testString.length)
+            // console.log("Test String:",input);
+            let mm = new MarkovMachine(input);
+            let createdText = mm.makeText()
+            // console.log("createdText:",createdText);
+            // Expectations
+            expect(createdText).toEqual(expect.any(String))
+            expect(createdText.split(/[ \r\n]+/).length).toEqual(100)
+        });
+
+
+            
+
+    });
 });
